663e48d96773f6d04c06dda23c6e685bc1eac51b
evanlin917 <102419050+evanlin917@users.noreply.github.com>
2023-01-07 20:25:00 -0500

    src/

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
         <h1>Evan's Blog:</h1>
         <div className="links">
            <a href="/">Home</a>
-           <a href="/create">New Blog</a> 
+           <a href="/create" style={{
+              color: "black",
+              backgroundColor: "#ffa500",
+              borderRadius: "8px"
+           }}>New Blog</a> 
         </div>
       </nav>  
     );
