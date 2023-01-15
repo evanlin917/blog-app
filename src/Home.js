53a30b2265a7c93b1d8f5213456954fff9f350f1
evanlin917 <102419050+evanlin917@users.noreply.github.com>
2023-01-15 14:48:22 -0500

    src/

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,9 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "Evan", id: 1 },
+    { title: "My new blog", body: "Hello there!", author: "Evan", id: 1 },
     { title: "Welcome to my blog!", body: "This is a blog where I write about the experiences of my days in college.", author: "Evan Lin", id: 2 },
-    { title: "Blog writing tips", body: "Below are outlined some common tips on how to write blogs!", author: "Evanski", id: 3 },
+    { title: "Life at Princeton", body: "In general, life at Princeton is pretty hard.", author: "Evanski", id: 3 },
     { title: "Hello Everyone! Nice to meet you!", body: "Hey everyone! Thanks for visiting this blog site, read more to get to know about me!", author: "Evanski Lin", id: 4 }
   ]);
 
@@ -20,7 +20,7 @@ const Home = () => {
     console.log("Use effect ran");
     console.log(blogs);
     console.log(name);
-  }, [name]);
+  }, []);
 
   return ( 
     <div className="home">
